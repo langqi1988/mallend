71eb1c9a21659e120b3b8cf5afad1657bf6f6884
langqi1988 <1171599077@qq.com>
2021-08-10 15:35:12 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,96 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <tab-bar>
+      <template #default>
+        <tab-bar-item
+          path='/home'
+          
+        >
+          <template v-slot:item-icon>
+            <img
+              src="./assets/img/tabbar/home.svg"
+              alt=""
+            >
+          </template>
+          <template v-slot:item-icon-active>
+            <img
+              src="./assets/img/tabbar/home_active.svg"
+              alt=""
+            >
+          </template>
+          <template #item-text>
+            <div>首页</div>
+          </template>
+        </tab-bar-item>
+        <tab-bar-item path='/category'>
+          <template v-slot:item-icon>
+            <img
+              src="./assets/img/tabbar/category.svg"
+              alt=""
+            >
+          </template>
+          <template v-slot:item-icon-active>
+            <img
+              src="./assets/img/tabbar/category_active.svg"
+              alt=""
+            >
+          </template>
+          <template #item-text>
+            <div>分类</div>
+          </template>
+        </tab-bar-item>
+        <tab-bar-item path='/cart'>
+          <template v-slot:item-icon>
+            <img
+              src="./assets/img/tabbar/shopcart.svg"
+              alt=""
+            >
+          </template>
+          <template v-slot:item-icon-active>
+            <img
+              src="./assets/img/tabbar/shopcart_active.svg"
+              alt=""
+            >
+          </template>
+          <template #item-text>
+            <div>购物车</div>
+          </template>
+        </tab-bar-item>
+        <tab-bar-item path='/profile'>
+          <template v-slot:item-icon>
+            <img
+              src="./assets/img/tabbar/profile.svg"
+              alt=""
+            >
+          </template>
+          <template v-slot:item-icon-active>
+            <img
+              src="./assets/img/tabbar/profile_active.svg"
+              alt=""
+            >
+          </template>
+          <template #item-text>
+            <div>我的</div>
+          </template>
+        </tab-bar-item>
+      </template>
+    </tab-bar>
   </div>
 </template>
 
+<script>
+import TabBar from "./components/tabbar/TabBar.vue";
+import TabBarItem from "./components/tabbar/TabBarItem.vue";
+export default {
+  name: "App",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import url("./assets/css/base.css");
 </style>
